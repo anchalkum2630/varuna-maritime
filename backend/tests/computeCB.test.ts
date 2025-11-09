8d9138fd79ebe2465a3a6358fcb52fbd4b7053b4
Anchal kumari <anchalkum2630@gmail.com>
2025-11-09 18:57:30 +0530

    Describe what you changed in version_1

--- backend/tests/computeCB.test.ts
+++ backend/tests/computeCB.test.ts
@@ -1,6 +1,6 @@
 import { computeCB } from "../src/core/application/computeCB";
 
-test("computeCB positive for R002 values", () => {
+test("computeCB positive for R002 values ", () => {
   const cb = computeCB(88.0, 4800);
   expect(typeof cb).toBe("number");
   expect(cb).toBeGreaterThan(0);
